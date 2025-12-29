06888a3856cbbf9b1eedb16e6844ab43c2f37ffd
gulggum <163222604+gulggum@users.noreply.github.com>
2025-12-29 09:49:37 +0900

    init:첫커밋

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-   
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>뷰쇼핑몰 시작</div>
 </template>
 
 <style scoped>
